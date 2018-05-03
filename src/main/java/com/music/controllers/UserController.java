3ed6e8b66d1464fc598c7284775231ae0109af1e
Vishwas <M1028002@mindtree.com>
2018-05-03 18:00:36 +0530

    autowiring repo and service

--- src/main/java/com/music/controllers/UserController.java
+++ src/main/java/com/music/controllers/UserController.java
@@ -1,15 +1,29 @@
 package com.music.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.music.entities.User;
+import com.music.services.UserService;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(value="http://localhost:4200")
 public class UserController {
+	
+	@Autowired
+	private UserService userService;
 
-//	@RequestMapping("/test")
-//	public String test() {
-//		System.out.println("working");
-//		return "integration successful";
-//	}
+	@RequestMapping(value="/register", method=RequestMethod.POST)
+	public ResponseEntity<?> registerUser(@RequestBody User bodyPayload) {
+		
+		System.out.println(bodyPayload);
+		
+		return null;
+	}
 }
--- src/main/java/com/music/services/UserService.java
+++ src/main/java/com/music/services/UserService.java
@@ -1,8 +1,17 @@
 package com.music.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.music.repositories.UserRepository;
 
 @Service
 public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
 
+	public void saveNewUser(){
+		
+	}
 }
