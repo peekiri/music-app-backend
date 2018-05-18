15d03bf1d1d3c56c246846d35ec46a2d546b145c
Vishwas <vishwasgv93@gmail.com>
2018-05-18 08:34:49 +0530

    Adding jwts in pom, adding filter

--- src/main/java/com/music/config/JWTAuthenticationFilter.java
+++ src/main/java/com/music/config/JWTAuthenticationFilter.java
@@ -1,18 +1,61 @@
 package com.music.config;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.music.entities.User;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
 
 	private AuthenticationManager authenticationManager;
 	
-	@Override
-	public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res) {
-		
+	public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
 	}
+	
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res) throws AuthenticationException{
+		try {
+			User user = new ObjectMapper().
+					readValue(req.getInputStream(), User.class);
+			
+			return authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(
+							user.getEmailAddress(),
+							user.getPassword(),
+							new ArrayList<>()));
+		} catch (RuntimeException | IOException e) {
+			throw new RuntimeException();
+		}
+	}
+	
+	@Override
+	public void successfulAuthentication(HttpServletRequest req,
+			HttpServletResponse res, FilterChain chain, 
+			Authentication auth) {
+		
+		String token = Jwts.builder()
+				.setSubject(((User)auth.getPrincipal()).getUserName())
+				.setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+				.signWith(SignatureAlgorithm.HS512, SecurityConstants.SECRET.getBytes())
+				.compact();
+		
+		res.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
+	}
+	
 }
--- src/main/java/com/music/config/SecurityConstants.java
+++ src/main/java/com/music/config/SecurityConstants.java
@@ -3,4 +3,8 @@ package com.music.config;
 public class SecurityConstants {
 
 	public static final String REGISTERURL = "/user/register";
+	public static final String SECRET = "LRPEEKIRI";
+    public static final long EXPIRATION_TIME = 43_199;
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
 }
